7d35c5b914b8d87f3be5e3b1dc7d6020b7e2fc6c
kimboyoon <kimboyoon0908@gmail.com>
2023-01-20 09:29:00 +0900

    Add

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Nav.scss';
 
 function Nav() {
   return (
     <div>
-      Nav<div>Let's GIT it</div>
+      Nav<div className="logo">Let's GIT it</div>
     </div>
   );
 }
