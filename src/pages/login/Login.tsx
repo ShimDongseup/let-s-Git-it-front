e3623bdc68fdafbc8e623eb7abdc00e82ba3aa47
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-01-23 23:25:42 +0900

    ADD : 로그인 모달창 레이아웃 구현

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import './Login.scss';
 
 function Login() {
-  return <div>Loginㅎㅎ</div>;
+  return (
+    <div className="wrapLogin">
+      <div className="wrapLoginBox">
+        <div className="loginLeft">
+          <img src="/images/loginImg.jpg" alt="loginImg" className="loginImg" />
+          <h2>환영합니다!</h2>
+        </div>
+        <div className="loginRight">
+          <div className="close material-symbols-outlined">close</div>
+          <div className="loginBtn">
+            <img
+              src="/images/github_logo.png"
+              alt="githubLogo"
+              className="githubLogo"
+            />
+            <span>로 로그인하기</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
