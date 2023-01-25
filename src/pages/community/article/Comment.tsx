f134665ba2012b560d3476319949ee69593f4632
kimboyoon <kimboyoon0908@gmail.com>
2023-01-25 13:32:22 +0900

    ADD: article layout

---
+++ src/pages/community/article/Comment.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Comment.scss';
+
+function Comment() {
+  return (
+    <div>
+      댓글 <div>댓글내용</div>
+      <textarea placeholder="댓글 남기기" />
+    </div>
+  );
+}
+
+export default Comment;
