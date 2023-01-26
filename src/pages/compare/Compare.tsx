b52dcc477ae512ba85b8616ffa9c17050bad2d9e
unknown <hsh1510@gmail.com>
2023-01-26 15:35:56 +0900

    ADD:data fetch

--- src/pages/compare/Compare.tsx
+++ src/pages/compare/Compare.tsx
@@ -3,11 +3,7 @@ import './Compare.scss';
 import Profile from '../../components/profile/Profile';
 
 function Compare() {
-  return (
-    <div>
-      <Profile />
-    </div>
-  );
+  return <div>{/* <Profile /> */}</div>;
 }
 
 export default Compare;
