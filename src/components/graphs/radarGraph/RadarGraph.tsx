35a8e1bee06fbe27804e17154b775f3cedbc33dc
unknown <hsh1510@gmail.com>
2023-01-26 22:55:43 +0900

    Modify:radarGraph element

--- src/components/graphs/radarGraph/RadarGraph.tsx
+++ src/components/graphs/radarGraph/RadarGraph.tsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import ReactApexChart from 'react-apexcharts';
 
-function ApexChart() {
+function RadarGraph() {
   return (
     <ReactApexChart
       // eslint-disable-next-line react/destructuring-assignment
       options={{
         chart: {
+          animations: {
+            enabled: true,
+            easing: 'easeinout',
+            speed: 1000,
+            animateGradually: {
+              enabled: true,
+              delay: 1000,
+            },
+            dynamicAnimation: {
+              enabled: true,
+              speed: 1000,
+            },
+          },
           height: 300,
           type: 'radar',
           dropShadow: {
@@ -26,9 +39,7 @@ function ApexChart() {
         markers: {
           size: 0,
         },
-        dataLabels: {
-          enabled: true,
-        },
+        dataLabels: { style: { fontSize: '10px' }, enabled: true },
         xaxis: {
           categories: ['호기심', '열정', '명성', '능력'],
         },
@@ -52,4 +63,4 @@ function ApexChart() {
   );
 }
 
-export default ApexChart;
+export default RadarGraph;
