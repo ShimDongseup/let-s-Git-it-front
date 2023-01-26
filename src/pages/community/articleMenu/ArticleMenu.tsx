b898d9f73dcd1212ece409c6b19b74b7dde182c9
ji <vyoungv7340naver.com>
2023-01-26 12:49:56 +0900

    css 리뷰 반영 및 디자인 컨벤션에 따른 수정

--- src/pages/community/articleMenu/ArticleMenu.tsx
+++ src/pages/community/articleMenu/ArticleMenu.tsx
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import INFO_CATEGORY_LIST from './InfoCategory';
+import './articleMenu.scss';
+import { useNavigate } from 'react-router-dom';
 
 function ArticleMenu() {
+  const [selectActive, setSelectActive] = useState<string>('');
+  const navigate = useNavigate();
+  const selectCategory = (e: React.MouseEvent<HTMLSpanElement>) => {
+    setSelectActive((e.target as HTMLElement).innerText);
+  };
   return (
     <div className="articleMenu">
       <div className="categoryListInner">
-        <div className="articleTop">---</div>
-        <div className="articleRegister">
-          <button className="articleWriteBtn">+ 글쓰기</button>
+        <div className="articleTop">
+          <img src="./image/icon/community.png" alt="community" />
         </div>
+        <div className="articleRegister">
+          <button
+            className="articleWriteBtn"
+            onClick={() => navigate('/articleWrite')}
+          >
+            + 글쓰기
+          </button>
+        </div>
+        {INFO_CATEGORY_LIST.map((main, i) => {
+          return (
+            <div className="categoryWrap" key={i}>
+              <h3 className="categoryTitle">{main.mainCategory}</h3>
+              <div className="categoryList">
+                {selectActive
+                  ? main.subTitle.map(sub => {
+                      return (
+                        <span
+                          onClick={selectCategory}
+                          key={sub.id}
+                          className={
+                            sub.title === selectActive ? 'categoryDefault' : ''
+                          }
+                        >
+                          {sub.title}
+                        </span>
+                      );
+                    })
+                  : main.subTitle.map(sub => {
+                      return (
+                        <span
+                          onClick={selectCategory}
+                          key={sub.id}
+                          className={
+                            sub.title === '자유' ? 'categoryDefault' : ''
+                          }
+                        >
+                          {sub.title}
+                        </span>
+                      );
+                    })}
+              </div>
+            </div>
+          );
+        })}
         <div className="articleSearch">
-          <h3 className="catergoryTitle">글 검색</h3>
+          <h3 className="categoryTitle">글 검색</h3>
           <select name="articleSelect" className="articleSelect">
             <option value="articleTitle">제목</option>
             <option value="articleWriter">글쓴이</option>
@@ -22,24 +73,6 @@ function ArticleMenu() {
               placeholder="내용을 입력하세요"
             />
             <button className="articleSearchBtn">검색</button>
-          </div>
-        </div>
-        <div className="categoryWrap">
-          <h3 className="catergoryTitle">정보</h3>
-          <div className="categoryList">
-            <span>공지사항</span>
-            <span>개발뉴스</span>
-            <span>버그/신고</span>
-          </div>
-        </div>
-        <div className="categoryWrap">
-          <h3 className="catergoryTitle">커뮤니티</h3>
-          <div className="categoryList">
-            <span className="categoryDefault">자유</span>
-            <span>유머</span>
-            <span>질문</span>
-            <span>프로젝트</span>
-            <span>채용정보</span>
           </div>
         </div>
       </div>
