0b20e2c102a2a574b621d2e4ba6e52766f6e27f0
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-01-27 16:15:28 +0900

    modify : padding 수정

--- src/pages/community/articleWrite/ArticleWrite.tsx
+++ src/pages/community/articleWrite/ArticleWrite.tsx
@@ -47,40 +47,42 @@ function ArticleWrite() {
   };
   return (
     <div className="wrapper">
-      <div className="wrapWrite">
-        <h2>글쓰기</h2>
-        <div className="choiceChange">
-          <div className="choiceMenu">
-            <div className="selected">
-              카테고리 선택
-              <div className="downIcon material-symbols-outlined">
-                expand_more
+      <div className="ptWrap">
+        <div className="wrapWrite">
+          <h2>글쓰기</h2>
+          <div className="choiceChange">
+            <div className="choiceMenu">
+              <div className="selected">
+                카테고리 선택
+                <div className="downIcon material-symbols-outlined">
+                  expand_more
+                </div>
               </div>
+              <ul className="dropDown">
+                <li>자유</li>
+                <li>유머</li>
+                <li>지문</li>
+                <li>프로젝트</li>
+                <li>채용정보</li>
+              </ul>
             </div>
-            <ul className="dropDown">
-              <li>자유</li>
-              <li>유머</li>
-              <li>지문</li>
-              <li>프로젝트</li>
-              <li>채용정보</li>
-            </ul>
           </div>
-        </div>
-        <input className="titleInput" type="text" placeholder="제목" />
-        <ReactQuill
-          className="textInput"
-          theme="snow"
-          placeholder={
-            '불법촬영물등을 게재할 경우 전기통신사업법 제22조의 5제1항에 따라 삭제,접속차단 등의 조치가 취해질 수 있으며 관련 법률에 따라 처벌받을 수 있습니다.'
-          }
-          modules={modules}
-          formats={formats}
-          // value={value}
-          // onChange={onChange}
-        />
-        <button className="cancleBtn">취소</button>
+          <input className="titleInput" type="text" placeholder="제목" />
+          <ReactQuill
+            className="textInput"
+            theme="snow"
+            placeholder={
+              '불법촬영물등을 게재할 경우 전기통신사업법 제22조의 5제1항에 따라 삭제,접속차단 등의 조치가 취해질 수 있으며 관련 법률에 따라 처벌받을 수 있습니다.'
+            }
+            modules={modules}
+            formats={formats}
+            // value={value}
+            // onChange={onChange}
+          />
+          <button className="cancleBtn">취소</button>
 
-        <button className="registerBtn">게시</button>
+          <button className="registerBtn">게시</button>
+        </div>
       </div>
     </div>
   );
--- src/pages/login/Signup.tsx
+++ src/pages/login/Signup.tsx
@@ -4,68 +4,70 @@ import './Signup.scss';
 function Signup() {
   return (
     <div className="wrapper">
-      <div className="wrapSignup">
-        <h1>환영합니다!</h1>
-        <h2>기본 회원 정보를 등록해주세요.</h2>
-        <div className="choice">
-          <h3 className="choiceTitle">국적을 선택해주세요</h3>
-          <div className="choiceChange">
-            <div className="choiceMenu">
-              <div className="selected">
-                국적
-                <div className="downIcon material-symbols-outlined">
-                  expand_more
+      <div className="ptWrap">
+        <div className="wrapSignup">
+          <h1>환영합니다!</h1>
+          <h2>기본 회원 정보를 등록해주세요.</h2>
+          <div className="choice">
+            <h3 className="choiceTitle">국적을 선택해주세요</h3>
+            <div className="choiceChange">
+              <div className="choiceMenu">
+                <div className="selected">
+                  국적
+                  <div className="downIcon material-symbols-outlined">
+                    expand_more
+                  </div>
                 </div>
+                <ul className="dropDown">
+                  <li>내국인</li>
+                  <li>FOREIGNER</li>
+                </ul>
               </div>
-              <ul className="dropDown">
-                <li>내국인</li>
-                <li>FOREIGNER</li>
-              </ul>
             </div>
           </div>
-        </div>
-        <div className="choice">
-          <h3 className="choiceTitle">개발분야를 선택해주세요</h3>
-          <div className="choiceChange">
-            <div className="choiceMenu">
-              <div className="selected">
-                개발분야
-                <div className="downIcon material-symbols-outlined">
-                  expand_more
+          <div className="choice">
+            <h3 className="choiceTitle">개발분야를 선택해주세요</h3>
+            <div className="choiceChange">
+              <div className="choiceMenu">
+                <div className="selected">
+                  개발분야
+                  <div className="downIcon material-symbols-outlined">
+                    expand_more
+                  </div>
                 </div>
+                <ul className="dropDown">
+                  <li>프로트엔드</li>
+                  <li>백엔드</li>
+                  <li>안드로이드</li>
+                  <li>ios</li>
+                </ul>
               </div>
-              <ul className="dropDown">
-                <li>프로트엔드</li>
-                <li>백엔드</li>
-                <li>안드로이드</li>
-                <li>ios</li>
-              </ul>
             </div>
           </div>
-        </div>
-        <div className="choice">
-          <h3 className="choiceTitle">경력을 선택해주세요</h3>
-          <div className="choiceChange">
-            <div className="choiceMenu">
-              <div className="selected">
-                경력
-                <div className="downIcon material-symbols-outlined">
-                  expand_more
+          <div className="choice">
+            <h3 className="choiceTitle">경력을 선택해주세요</h3>
+            <div className="choiceChange">
+              <div className="choiceMenu">
+                <div className="selected">
+                  경력
+                  <div className="downIcon material-symbols-outlined">
+                    expand_more
+                  </div>
                 </div>
+                <ul className="dropDown">
+                  <li>학생</li>
+                  <li>신입</li>
+                  <li>1년차</li>
+                  <li>2년차</li>
+                  <li>3년차</li>
+                </ul>
               </div>
-              <ul className="dropDown">
-                <li>학생</li>
-                <li>신입</li>
-                <li>1년차</li>
-                <li>2년차</li>
-                <li>3년차</li>
-              </ul>
             </div>
           </div>
-        </div>
-        <div className="wrapBtn">
-          <button className="cancleBtn">취소</button>
-          <button className="registerBtn">등록</button>
+          <div className="wrapBtn">
+            <button className="cancleBtn">취소</button>
+            <button className="registerBtn">등록</button>
+          </div>
         </div>
       </div>
     </div>
