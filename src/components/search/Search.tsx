86d40431a61d0b8f6f225ba77460d22becd344f5
kimboyoon <kimboyoon0908@gmail.com>
2023-01-30 21:21:26 +0900

    Add: axios

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 import { FaSearch } from 'react-icons/fa';
 import './Search.scss';
 
 function Search({ size }: any) {
   const [search, setSearch] = useState<string>('');
+  const [results, setResults] = useState<string>('');
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
+    axios
+      .get(`/ranks/search?user-name=${search}`)
+      .then(res => setResults(res.data));
   };
 
   return (
@@ -24,21 +30,29 @@ function Search({ size }: any) {
       {search && (
         <div className={`resultWrap ${size}`}>
           검색결과
-          <div className="resultList">
-            {SEARCH_RESULT_DATAS.map(data => {
-              return (
-                <div className={`resultInfo ${size}`} key={data.id}>
-                  <img
-                    className={`img ${size}`}
-                    src="https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg"
-                    alt="profile Img"
-                  />
-                  <div className="tier">Tier</div>
-                  <div>kby0908</div>
-                </div>
-              );
-            })}
-          </div>
+          {results ? (
+            <div className="resultList">
+              {SEARCH_RESULT_DATAS.map(data => {
+                return (
+                  <Link
+                    className={`resultInfo ${size}`}
+                    to="/userdetail"
+                    key={data.id}
+                  >
+                    <img
+                      className={`img ${size}`}
+                      src="https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg"
+                      alt="profile Img"
+                    />
+                    <div className="tier">Tier</div>
+                    <div>kby0908</div>
+                  </Link>
+                );
+              })}
+            </div>
+          ) : (
+            <div className={`errorMsg ${size}`}>알맞은 유저가 없습니다</div>
+          )}
         </div>
       )}
     </div>
