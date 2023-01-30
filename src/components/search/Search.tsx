997e13aa47edb26de04d84fcd5f8fec5fb7531e1
kimboyoon <kimboyoon0908@gmail.com>
2023-01-30 17:40:00 +0900

    Add: search result

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,19 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import './Search.scss';
 
-function Search({ children, size }: any) {
+function Search({ size }: any) {
+  const [search, setSearch] = useState<string>('');
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className={`search ${size}`}>
-      {children}
-      <input
-        className={`searchInput ${size}`}
-        type="search"
-        placeholder="유저 검색"
-      />
-      <FaSearch className={`searchIcon ${size}`} />
+      <form className={`searchForm ${size}`}>
+        <input
+          className={`searchInput ${size}`}
+          value={search}
+          onChange={handleInput}
+          type="search"
+          placeholder="유저 검색"
+        />
+        <FaSearch className={`searchIcon ${size}`} />
+      </form>
+      {search && (
+        <div className={`resultWrap ${size}`}>
+          검색결과
+          <div className="resultList">
+            {SEARCH_RESULT_DATAS.map(data => {
+              return (
+                <div className={`resultInfo ${size}`} key={data.id}>
+                  <img
+                    className={`img ${size}`}
+                    src="https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg"
+                    alt="profile Img"
+                  />
+                  <div className="tier">Tier</div>
+                  <div>kby0908</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Search;
+
+const SEARCH_RESULT_DATAS = [
+  {
+    id: 1,
+    img: 'https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg',
+    name: 'kby008',
+  },
+  {
+    id: 2,
+    img: 'https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg',
+    name: 'kby008',
+  },
+  {
+    id: 3,
+    img: 'https://cdn.pixabay.com/photo/2018/05/13/16/57/dog-3397110__480.jpg',
+    name: 'kby008',
+  },
+];
