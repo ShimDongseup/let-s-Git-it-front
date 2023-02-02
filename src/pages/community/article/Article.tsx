eab0e600c87178b4136bfbf96f61b57f118f08cd
kimboyoon <kimboyoon0908@gmail.com>
2023-02-03 01:00:59 +0900

    Add: article, comment axios

--- src/pages/community/article/Article.tsx
+++ src/pages/community/article/Article.tsx
@@ -1,51 +1,62 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { FaRegThumbsUp, FaThumbsUp, FaRegComment } from 'react-icons/fa';
 import { HiOutlineShare } from 'react-icons/hi';
 import { AiOutlineAlert } from 'react-icons/ai';
 import Comment from '../article/Comment';
-import CommentList from './CommentList';
 import ArticleMenu from '../articleMenu/ArticleMenu';
 import './Article.scss';
 
 type ArticleData = {
-  id: string;
-  header: string;
+  id: number;
+  title: string;
   content: string;
-  writer: string;
+  userName: string;
+  subCategory: string;
   tier: string;
-  commentNum: number;
-  likesNum: number;
+  createdAt: string;
+  like: LikeData[];
+};
+
+type LikeData = {
+  likeId: number;
+  userId: number;
+  createdAt: string;
 };
 
 function Article() {
-  const [article, setArticle] = useState<ArticleData[]>([]);
-  const [likes, setLikes] = useState<number>(5); // 초기값 article.likesNum
+  const [article, setArticle] = useState<ArticleData[]>(ARTICLE_DATA);
+  const { title, content, tier, userName, subCategory, createdAt, like } =
+    article[0];
+  const [likes, setLikes] = useState<number>(like.length);
   const [isCheckLikes, setIsCheckLikes] = useState<boolean>(false);
 
   const postId = useParams<string>();
   const token = localStorage.getItem('token');
 
-  const loadArticle = () => {
-    axios
-      .get(`/community/posts/${postId}`, {
-        headers: {
-          token: token,
-        },
-      })
-      .then(res => setArticle(res.data))
-      .catch(err => {
-        if (err.response) {
-          console.log(err.response);
-        } else if (err.request) {
-          console.log(err.request);
-        } else {
-          console.log('err', err.message);
-        }
-      });
+  // 게시글 조회
+  const loadArticle = async () => {
+    // await axios
+    //     .get('/data/article.json')
+    //     // .get(`/community/posts/${postId}`, {
+    //     //   headers: {
+    //     //     token: token,
+    //     //   },
+    //     // })
+    //     .then(res => setArticle(res.data))
+    //     .catch(err => {
+    //       if (err.response) {
+    //         console.log(err.response);
+    //       } else if (err.request) {
+    //         console.log(err.request);
+    //       } else {
+    //         console.log('err', err.message);
+    //       }
+    //     });
   };
 
+  // 게시글 좋아요
   const loadLikesNum = () => {
     axios
       .post(`community/likes/${postId}`, {
@@ -65,76 +76,111 @@ function Article() {
     loadLikesNum();
   };
 
+  // 글 삭제하기
+  const deleteArticle = () => {
+    axios
+      .delete(`/community/posts/${postId}`, {
+        headers: {
+          token: token,
+        },
+      })
+      .then(res => alert('삭제하시겠습니까?'))
+      .catch(err => console.log(err));
+  };
+
   useEffect(() => {
-    loadArticle();
+    // loadArticle();
   }, []);
 
   return (
-    <div className="articlePage">
-      <div className="listAndArticle">
-        <ArticleMenu />
-        <div className="articleWrap">
-          <header className="headerWrap">
-            <div className="title">제목제목제목제목</div>
-            <div className="titleInner">
-              <ul>
-                <li className="category">카테고리</li>
-                <li className="slash">|</li>
-                <li className="date">2023.01.26</li>
-                <li className="slash">|</li>
-                <li className="writer">글쓴이</li>
-              </ul>
-            </div>
-          </header>
-          <main className="mainWrap">
-            <div className="article">
-              다 나는 별이 까닭입니다. 가을로 내 된 계절이 별들을 별 있습니다.
-              그리고 청춘이 가난한 걱정도 프랑시스 이름자 버리었습니다. 별에도
-              새겨지는 다 밤을 별 보고, 버리었습니다. 어머니 아스라히 별 릴케
-              보고, 까닭입니다. 까닭이요, 이름을 노루, 아스라히 너무나 나는 차
-              하나에 불러 계십니다. 헤는 이국 강아지, 가득 불러 내린 불러
-              버리었습니다. 멀듯이, 오는 별 까닭입니다. 이런 봄이 오는 무엇인지
-              이름과 노새, 했던 딴은 거외다. 별빛이 하나 없이 겨울이 하나에 벌써
-              너무나 있습니다. 된 둘 경, 하나에 있습니다. <br />
-              위에도 어머니, 불러 헤는 말 까닭입니다. 어머니 별에도 지나가는
-              자랑처럼 오면 마디씩 가득 계십니다. 파란 가득 못 된 하나에 가난한
-              있습니다. 아무 못 위에 강아지, 별 한 말 이름과 내일 있습니다. 불러
-              책상을 헤일 소녀들의 위에도 릴케 북간도에 봅니다. 하늘에는
-              북간도에 못 봅니다. 겨울이 별들을 무엇인지 봅니다. 하나 까닭이요,
-              가득 밤이 별 아침이 겨울이 소학교 이네들은 까닭입니다. 까닭이요,
-              이름과,
-            </div>
-            <section className="mainBottom">
-              <div className="thumsCommentIcons">
-                <div className="thumbsUpWrap">
-                  {isCheckLikes === true ? (
-                    <FaThumbsUp className="thumbsUp" onClick={clickThumbsUp} />
-                  ) : (
-                    <FaRegThumbsUp
-                      className="thumbsUp"
-                      onClick={clickThumbsUp}
-                    />
-                  )}
-
-                  <span>{likes}</span>
-                </div>
-                <div className="commentIconWrap">
-                  <FaRegComment className="comment" />
-                  <span>15</span>
-                </div>
+    article && (
+      <div className="articlePage">
+        <div className="listAndArticle">
+          <ArticleMenu />
+          <div className="articleWrap">
+            <header className="headerWrap">
+              <div className="titleWrap">
+                <div className="title">{title}</div>
+                <ul className="editDel">
+                  <li className="edit">수정</li>
+                  <li className="del" onClick={deleteArticle}>
+                    삭제
+                  </li>
+                </ul>
               </div>
-              <div className="shareSirenIcons">
-                <HiOutlineShare className="share" />
-                <AiOutlineAlert className="siren" />
+              <div className="titleInner">
+                <ul>
+                  <li>{subCategory}</li>
+                  <li className="slash">|</li>
+                  <li>{createdAt}</li>
+                  <li className="slash">|</li>
+                  <li className="tier">{tier}</li>
+                  <li>{userName}</li>
+                </ul>
               </div>
-            </section>
-          </main>
-          <Comment />
-          <CommentList />
+            </header>
+            <main className="mainWrap">
+              <div className="article">{content}</div>
+              <section className="mainBottom">
+                <div className="thumsCommentIcons">
+                  <div className="thumbsUpWrap">
+                    {isCheckLikes === true ? (
+                      <FaThumbsUp
+                        className="thumbsUp"
+                        onClick={clickThumbsUp}
+                      />
+                    ) : (
+                      <FaRegThumbsUp
+                        className="thumbsUp"
+                        onClick={clickThumbsUp}
+                      />
+                    )}
+                    <span>{likes}</span>
+                  </div>
+                  <div className="commentIconWrap">
+                    <FaRegComment className="comment" />
+                    <span>15</span>
+                  </div>
+                </div>
+                <div className="shareSirenIcons">
+                  <HiOutlineShare className="share" />
+                  <AiOutlineAlert className="siren" />
+                </div>
+              </section>
+            </main>
+            <Comment />
+            {/* <CommentList /> */}
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
 export default Article;
+
+const ARTICLE_DATA = [
+  {
+    id: 1,
+    postId: 1,
+    title: '글 제목',
+    content:
+      '다 나는 별이 까닭입니다.별에도 새겨지는 다 밤을 보고 버리었습니다. 어머니 아스라히 별 보고 까닭입니다. 파란 가득 못 된 하나에 가난한 강아지 별 한 말과 이름 내일 있습니다.',
+    userName: 'kby0908',
+    subCategory: '자유',
+    createdAt: '2023-02-02',
+    tier: 'tier',
+    like: [
+      {
+        likeId: 1,
+        userId: 1,
+        createdAt: '2023-02-02',
+      },
+      {
+        likeId: 2,
+        userId: 2,
+        createdAt: '2023-02-02',
+      },
+    ],
+  },
+];
--- src/pages/community/article/Comment.tsx
+++ src/pages/community/article/Comment.tsx
@@ -1,42 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaCaretRight } from 'react-icons/fa';
 import './Comment.scss';
+import axios from 'axios';
+import { useParams } from 'react-router';
+import CommentList from './CommentList';
 
 function Comment() {
+  const [comment, setComment] = useState('');
   const token = localStorage.getItem('token');
+  const postId = useParams();
+  const valid = comment ? false : true;
+
+  // 댓글 등록하기
+  const handleComment = async (e: any) => {
+    setComment(e.target.value);
+
+    await axios
+      .post(`/community/posts/${postId}/comment`, {
+        headers: { token: token },
+        data: { comment: comment },
+      })
+      .then(res => alert('성공!'))
+      .catch(err => alert(err));
+  };
+
+  console.log(comment);
 
   return (
-    <div className="commentPage">
-      {token ? (
-        <>
-          <section className="userInfo">
-            <img
-              className="profileImg"
-              src="https://cdn.pixabay.com/photo/2014/08/10/08/17/dog-414570__480.jpg"
-              alt="profile img"
-            />
-            <div className="tier">Tier</div>
-            <div className="userName">김보윤</div>
+    <>
+      <div className="commentPage">
+        {!token ? (
+          <>
+            <section className="userInfo">
+              <img
+                className="profileImg"
+                src="https://cdn.pixabay.com/photo/2014/08/10/08/17/dog-414570__480.jpg"
+                alt="profile img"
+              />
+              <div className="tier">Tier</div>
+              <div className="userName">김보윤</div>
+            </section>
+            <form className="comment">
+              <textarea
+                className="commentInput"
+                placeholder="댓글 남기기"
+                onChange={handleComment}
+              />
+              <div className="enroll">
+                <button
+                  className={!valid ? 'enrollBtn active' : 'enrollBtn'}
+                  disabled={valid}
+                >
+                  등록
+                </button>
+              </div>
+            </form>
+          </>
+        ) : (
+          <section className="loginMsg">
+            <FaCaretRight className="icon" />
+            댓글을 남기시려면
+            <Link to="/login" className="goToLogin">
+              로그인
+            </Link>
+            하세요
           </section>
-          <form className="comment">
-            <textarea className="commentInput" placeholder="댓글 남기기" />
-            <div className="enroll">
-              <button className="enrollBtn">등록</button>
-            </div>
-          </form>
-        </>
-      ) : (
-        <section className="loginMsg">
-          <FaCaretRight className="icon" />
-          댓글을 남기시려면
-          <Link to="/login" className="goToLogin">
-            로그인
-          </Link>
-          하세요
-        </section>
-      )}
-    </div>
+        )}
+      </div>
+      <CommentList />
+    </>
   );
 }
 
