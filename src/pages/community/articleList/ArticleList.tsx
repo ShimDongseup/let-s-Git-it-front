fc5b748e8499a5a5d1d00da1ca7c639e17ffa3b1
ji <vyoungv7340naver.com>
2023-02-06 21:50:58 +0900

    articleList function 피드백 반영

--- src/pages/community/articleList/ArticleList.tsx
+++ src/pages/community/articleList/ArticleList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { SetStateAction } from 'react';
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import Pagination from 'react-js-pagination';
@@ -27,22 +27,52 @@ function ArticleList() {
   const [selectedMain, setSelectedMain] = useState<string>('');
   const [selectActive, setSelectActive] = useState<number>(4);
   const [articleCopyList, setArticleCopyList] = useState<ArticleType[]>([]);
+  const [selectedHotDate, setSelectedHotDate] = useState('전체');
+  const [hotSortList, setHotSortList] = useState<ArticleType[]>([]);
+
+  // sort 분류기
+  const comparator = (name: string): any => {
+    return (prev: { [x: string]: number }, next: { [x: string]: number }) =>
+      prev[name] === next[name] ? 0 : prev[name] > next[name] ? -1 : 1;
+  };
+
+  // 인기순 정렬
+  const [isLatest, setIsLatest] = useState<boolean>(true);
+  const today = new Date().getTime();
+  const hotSortDate = (id: number) => {
+    let hotDateList: ArticleType[] = [];
+    hotSortList.forEach(el => {
+      let postDate = new Date(el.postTime).getTime();
+      let interval = Math.floor((today - postDate) / (1000 * 60 * 60 * 24));
+      if (interval < id) {
+        hotDateList.push(el);
+      }
+    });
+    console.log(1);
+    setArticleList(hotDateList);
+  };
+  const handleHotDate = (e: { target: { value: SetStateAction<string> } }) => {
+    setSelectedHotDate(e.target.value);
+    hotSortDate(Number(e.target.value));
+  };
 
   // 최신순, 인기순 클릭
-  const [isLatest, setIsLatest] = useState<boolean>(true);
   const clickMenutab = () => {
     setIsLatest(!isLatest);
     if (isLatest) {
       const hotList = [...articleList];
-      const comparator = (name: string): any => {
-        return (prev: { [x: string]: number }, next: { [x: string]: number }) =>
-          prev[name] === next[name] ? 0 : prev[name] > next[name] ? -1 : 1;
-      };
       hotList.sort(comparator('recommendNum'));
       setArticleList(hotList);
+      setHotSortList(hotList);
     } else {
       setArticleList(articleCopyList);
     }
+  };
+
+  // 인기순 초기화
+  const intialization = () => {
+    setSelectedHotDate('전체');
+    setArticleList(hotSortList);
   };
 
   // 글검색 결과 fetch
@@ -113,14 +143,36 @@ function ArticleList() {
                   className={isLatest ? 'selectTab' : ''}
                   onClick={clickMenutab}
                 >
-                  &nbsp;최근&nbsp;
+                  &nbsp;최신순&nbsp;
                 </span>
                 <span
                   className={isLatest ? '' : 'selectTab'}
                   onClick={clickMenutab}
                 >
-                  &nbsp;인기&nbsp;
+                  &nbsp;인기순&nbsp;
                 </span>
+                {!isLatest && (
+                  <div>
+                    <button className="initialHotBtn" onClick={intialization}>
+                      <img src="./image/icon/return.png" alt="undo" />
+                    </button>
+                    <select
+                      className="hotSelect"
+                      value={selectedHotDate}
+                      onChange={handleHotDate}
+                    >
+                      {HOT_DATE_LIST.map(
+                        (date: { id: number; date: string }) => {
+                          return (
+                            <option value={date.id} key={date.id}>
+                              {date.date}
+                            </option>
+                          );
+                        }
+                      )}
+                    </select>
+                  </div>
+                )}
               </>
             )}
           </div>
@@ -152,3 +204,11 @@ function ArticleList() {
 }
 
 export default ArticleList;
+
+const HOT_DATE_LIST: { id: number; date: string }[] = [
+  { id: 366, date: '전체' },
+  { id: 1, date: '오늘' },
+  { id: 7, date: '일주일' },
+  { id: 30, date: '한달' },
+  { id: 365, date: '일년' },
+];
