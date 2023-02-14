040defa11cbc9f1d8db74f5680287a00b7895b05
kimboyoon <kimboyoon0908@gmail.com>
2023-02-14 19:03:25 +0900

    Add: css

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -3,11 +3,13 @@ import './NotFound.scss';
 
 const NotFound = () => {
   return (
-    <div className="eee">
-      <div className="errorPage">
-        <div className="errorWrap">
-          <div className="errMsg">404 Error</div>
-          <div className="notFound">주소를 확인해주세요</div>
+    <div className="errorPage">
+      <div className="errorWrap">
+        <div className="logo">let's GIT it!</div>
+        <div className="errMsg">404 Not found</div>
+        <div className="noPage">페이지가 존재하지 않습니다.</div>
+        <div className="detailMsg">
+          링크를 잘못 입력하셨거나 페이지가 삭제/이동되었을 수 있습니다.
         </div>
       </div>
     </div>
