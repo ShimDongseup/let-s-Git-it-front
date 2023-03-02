3ae20437613a3dcfe74a117a7958db7fc2a0bcee
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-03-02 12:30:07 +0900

    server test 오타수정

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.lets-git-it.online/';
+export const BASE_URL = 'https://api.lets-git-it.online';
 export const TOKEN = `Bearer ${localStorage.getItem('token')}`;
 
 export const HEADERS = { headers: { Authorization: `${TOKEN}` } };
