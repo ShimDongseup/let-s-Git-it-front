50f1f670d80fe4ee541a26525e6a8faa10dc8046
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-03-02 21:51:28 +0900

    recoil-persist 패키지 삭제

--- src/atom.tsx
+++ src/atom.tsx
@@ -1,7 +1,4 @@
 import { atom } from 'recoil';
-import { recoilPersist } from 'recoil-persist';
-
-const { persistAtom } = recoilPersist();
 
 const categoryState = atom({
   key: 'categoryState',
@@ -23,16 +20,9 @@ const currentPage = atom({
   default: 1,
 });
 
-const loginState = atom({
-  key: 'loginState',
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
-
 export {
   categoryState,
   articleSearchOption,
   articleSearchKeyword,
   currentPage,
-  loginState,
 };
