e3210afedaed87df5a9e55dcbd7bf11e3d319a50
unknown <hsh1510@gmail.com>
2023-04-21 13:49:40 +0900

    Modify:login

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://api.lets-git-it.online'; // 개발서버
+export const BASE_URL = 'https://api.lets-git-it.online';
 export const TOKEN = `Bearer ${localStorage.getItem('token')}`;
 export const HEADERS = { headers: { Authorization: `${TOKEN}` } };
