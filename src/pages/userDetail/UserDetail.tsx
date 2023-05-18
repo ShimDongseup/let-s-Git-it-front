8df5f0f227dcd7fbfd2b02841b30ae11b83e32d2
동섭 <DONGSEUP@dongseobs-MacBookPro.local>
2023-05-18 16:34:50 +0900

    잔디 api 엔드포인트 및 프록시 수정

--- src/pages/userDetail/UserDetail.tsx
+++ src/pages/userDetail/UserDetail.tsx
@@ -163,15 +163,13 @@ function UserDetail() {
 
   useEffect(() => {
     //잔디 api
-    axios
-      .get(`https://github-contributions-api.jogruber.de/v4/${userName}?y=last`)
-      .then(result => {
-        if (result.data === 404) {
-          setIsMounted(false);
-        } else {
-          setIsMounted(true);
-        }
-      });
+    axios.get(`/v4/${userName}?y=last`).then(result => {
+      if (result.data === 404) {
+        setIsMounted(false);
+      } else {
+        setIsMounted(true);
+      }
+    });
   }, []);
 
   const recall = () => {
