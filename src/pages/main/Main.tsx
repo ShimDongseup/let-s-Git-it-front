d325d3fb2f23d81270b2851e316ab450b2686534
kimboyoon <kimboyoon0908@gmail.com>
2023-05-19 01:20:44 +0900

    Delete server err msg

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,20 +2,13 @@ import React from 'react';
 import Top5 from './Top5';
 import Search from '../../components/search/Search';
 import { AiOutlineGithub } from 'react-icons/ai';
-import { BiError } from 'react-icons/bi';
 import './Main.scss';
 
 function Main() {
   return (
     <main className="mainPage">
       <section className="mainWrap">
-        <article className="serverWrap">
-          <BiError className="errIcon" />
-          <h1 className="logo">let's GIT it</h1>
-          <h2>서버 점검 작업으로 사이트 이용이 불가합니다.</h2>
-          <h2>이용에 불편을 드려 죄송합니다.</h2>
-        </article>
-        {/* <article className="logoWrap">
+        <article className="logoWrap">
           <h1 className="logo">let's GIT it!</h1>
           <h2 className="info">
             <div className="github">
@@ -31,7 +24,7 @@ function Main() {
           </section>
         </article>
         <h1 className="top5Header">TOP 5</h1>
-        <Top5 /> */}
+        <Top5 />
       </section>
     </main>
   );
