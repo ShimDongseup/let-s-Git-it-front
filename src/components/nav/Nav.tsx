8bb5c516818399c01f556404010381aaf4e9b04e
동섭 <sk71927170@gmail.com>
2023-09-01 00:00:01 +0900

    Modify : 모바일버전 로그인오류 해결

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -12,7 +12,7 @@ function Nav() {
   const [activeLogin, setActivelogin] = useState(false);
 
   const handleLogin = (): void => {
-    window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_REST__KEY}&redirect_uri=https://let-s-git-it.vercel.app/githublogin`;
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_GITHUB_REST_API_KEY}&redirect_uri=https://let-s-git-it.vercel.app/githublogin`;
     localStorage.setItem('referrer', window.location.href);
   };
 
